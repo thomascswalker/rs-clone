38489103cba10369f69b34ce255a1a8c5c4b9877
thomascswalker <thomascswalker@gmail.com>
2021-09-28 15:20:05 -0400

    Got OpenGL to work

---
+++ source/rsclone/main.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<glad/gl.h>
+#include<GLFW/glfw3.h>
+
+int main()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	int sizeX = 500;
+	int sizeY = 500;
+
+	GLFWwindow* window = glfwCreateWindow(sizeX, sizeY, "RS Clone", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL(glfwGetProcAddress);
+
+	glViewport(0, 0, sizeX, sizeY);
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
