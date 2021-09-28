abf672e18973df1ae3cb641eea342bc3fc5ae134
thomascswalker <thomascswalker@gmail.com>
2021-09-28 18:12:44 -0400

    Add glm

--- source/rsclone/main.cpp
+++ source/rsclone/main.cpp
@@ -3,6 +3,10 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "shaderclass.h"
 #include "vao.h"
 #include "vbo.h"
