619b33eeeaa163f3d5741e1d7c47ea746a021982
thomascswalker <thomascswalker@gmail.com>
2021-10-04 16:13:20 -0400

    Got model loading to work!
    TODO:
    
    - Resolve vertex colors (currently it's using normals as color)
    - Models import on another axis

--- source/rsclone/main.cpp
+++ source/rsclone/main.cpp
@@ -1,14 +1,33 @@
-#include <filesystem>
-#include "Model.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "shader.h"
+#include "camera.h"
+#include "filesystem.h"
+#include "model.h"
+
+// camera
+bool firstMouse = true;
 
 const unsigned int width = 800;
 const unsigned int height = 800;
 
-Camera camera(width, height);
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = width / 2.0f;
+float lastY = height / 2.0f;
 
 void ProcessInput(GLFWwindow* window);
 void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
 void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+void MouseCallback(GLFWwindow* window, double xpos, double ypos);
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
 int main()
 {
@@ -19,6 +38,7 @@ int main()
 	// In this case we are using OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
 	// Tell GLFW we are using the CORE profile
 	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -37,50 +57,62 @@ int main()
 	glfwMakeContextCurrent(window);
 	glfwSetScrollCallback(window, ScrollCallback);
 	glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+	glfwSetCursorPosCallback(window, MouseCallback);
+
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	//Load GLAD so it configures OpenGL
-	gladLoadGL(glfwGetProcAddress);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
 
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
 	glViewport(0, 0, width, height);
 
 	// Generates Shader object using shaders default.vert and default.frag
-	Shader shaderProgram("default.vert", "default.frag");
-
-	// Take care of all the light related things
-	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
-	glm::mat4 lightModel = glm::mat4(1.0f);
-	lightModel = glm::translate(lightModel, lightPos);
-
-	shaderProgram.Activate();
-	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
-	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+	Shader shader("default.vert", "default.frag");
+	shader.use();
 
 	// Enables the Depth Buffer
 	glEnable(GL_DEPTH_TEST);
 
 	// Load in a model
-	std::string parentDir = (std::filesystem::current_path().std::filesystem::path::parent_path()).string();
-	std::string modelPath = "/assets/prod/scene.gltf";
-	Model model((parentDir + modelPath).c_str());
-
-	// Specify the color of the background
-	glClearColor(0.18f, 0.18f, 0.18f, 1.0f);
+	Model model(FileSystem::getPath("../assets/prod/scene.fbx"));
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
 
-		// Clean the back buffer and depth buffer
+		glClearColor(0.07f, 0.20f, 0.25f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Handles camera inputs
 		ProcessInput(window);
+		
+		// Draw our model
+		// view/projection transformations
+		float nearClip = 0.01f;
+		float farClip = 10000.0f;
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)width / (float)height, nearClip, farClip);
+		glm::mat4 view = camera.GetViewMatrix();
+		shader.setMat4("projection", projection);
+		shader.setMat4("view", view);
 
-		// Draw a model
-		model.Draw(shaderProgram, camera);
+		// render the loaded model
+		glm::mat4 temp = glm::mat4(1.0f);
+		//temp = glm::rotate(temp, -90.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+		//temp = glm::translate(temp, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
+		//temp = glm::scale(temp, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+		shader.setMat4("model", temp);
+		
+		model.Draw(shader);
 
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
@@ -89,24 +121,32 @@ int main()
 		glfwPollEvents();
 	}
 
-	// Delete all the objects we've created
-	shaderProgram.Delete();
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
+
 	// Terminate GLFW before ending the program
 	glfwTerminate();
+
 	return 0;
 }
 
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
 void ProcessInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-	{
 		glfwSetWindowShouldClose(window, true);
-	}
 
-	camera.Inputs(window);
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
 }
+
 
 void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
 {
@@ -118,4 +158,24 @@ void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
 	// make sure the viewport matches the new window dimensions; note that width and 
 	// height will be significantly larger than specified on retina displays.
 	glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void MouseCallback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset, true);
 }
