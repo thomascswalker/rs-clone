ecf8240ce6624468fc6ee715c28200eb85c1a145
thomascswalker <thomascswalker@gmail.com>
2021-10-05 11:57:01 -0400

    Added basic framebuffer

--- source/rsclone/main.cpp
+++ source/rsclone/main.cpp
@@ -1,5 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <GL/GLU.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -11,17 +12,19 @@
 #include "model.h"
 
 // camera
+GLfloat SKYBOX[] = { 0.25f, 0.5f, 0.75f, 1.0f };
 bool firstMouse = true;
 
-const unsigned int width = 800;
-const unsigned int height = 800;
+const unsigned int WIDTH = 1280;
+const unsigned int HEIGHT = 720;
+const float ASPECT = float(WIDTH) / HEIGHT;
 
 float nearClip = 1.0f;
-float farClip = 5000.0f;
+float farClip = 10000.0f;
 
 Camera camera(glm::vec3(0.0f, 50.0f, 3.0f));
-float lastX = width / 2.0f;
-float lastY = height / 2.0f;
+float lastX = WIDTH / 2.0f;
+float lastY = HEIGHT / 2.0f;
 
 void ProcessInput(GLFWwindow* window);
 void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
@@ -47,7 +50,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(width, height, "RS Clone", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "RS Clone", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
@@ -74,57 +77,148 @@ int main()
 
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, width, height);
+	glViewport(0, 0, WIDTH, HEIGHT);
 	glMatrixMode(GL_MODELVIEW);
 
-	// Generates Shader object using shaders default.vert and default.frag
-	Shader shader("default.vert", "default.frag");
-	shader.use();
 
-	// Enables the Depth Buffer
-	glEnable(GL_DEPTH_TEST);
+
+
+
+	//// ------- MODELS ------- ////
 
 	// Load in a model
 	Model model(FileSystem::getPath("../assets/prod/scene.fbx"));
 	Model player(FileSystem::getPath("../assets/prod/playercharacter.fbx"));
 
-	// Main while loop
+
+
+
+	//// ------- FRAMEBUFFER -------- ////
+	
+	float screenVertices[] = { // vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+	// positions   // texCoords
+		-1.0f,  1.0f,  0.0f, 1.0f,
+		-1.0f, -1.0f,  0.0f, 0.0f,
+		 1.0f, -1.0f,  1.0f, 0.0f,
+
+		-1.0f,  1.0f,  0.0f, 1.0f,
+		 1.0f, -1.0f,  1.0f, 0.0f,
+		 1.0f,  1.0f,  1.0f, 1.0f
+	};
+
+	// screen quad VAO
+	unsigned int quadVAO, quadVBO;
+	glGenVertexArrays(1, &quadVAO);
+	glGenBuffers(1, &quadVBO);
+	glBindVertexArray(quadVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(screenVertices), &screenVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+	// Framebuffer
+	unsigned int framebuffer;
+	glGenFramebuffers(1, &framebuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+	// Texture color buffer
+	unsigned int textureColorbuffer;
+	glGenTextures(1, &textureColorbuffer);
+	glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WIDTH, HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+
+	// Renderbuffer
+	unsigned int renderbuffer;
+	glGenRenderbuffers(1, &renderbuffer);
+	glBindRenderbuffer(GL_RENDERBUFFER, renderbuffer);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, WIDTH, HEIGHT);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderbuffer);
+
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+	{
+		std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+	}
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+
+
+	//// ------- SHADERS -------- ////
+
+	// Generates Shader object using shaders default.vert and default.frag
+	Shader defaultShader("default.vert", "default.frag");
+	defaultShader.use();
+
+	Shader screenShader("screen.vert", "screen.frag");
+	screenShader.use();
+
+
+
+	//// ------- MAIN GAME LOOP -------- ////
+
 	while (!glfwWindowShouldClose(window))
 	{
+		// Per-frame logic
 		float currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-		glClearColor(0.01f, 0.01f, 0.01f, 1.0f);
+		// Camera input
+		ProcessInput(window);
+
+		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+		glEnable(GL_DEPTH_TEST); // enable depth testing (is disabled for rendering screen-space quad)
+
+		glClearColor(SKYBOX[0], SKYBOX[1], SKYBOX[2], SKYBOX[3]);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// Handles camera inputs
-		ProcessInput(window);
-		
+
+		//// ------ DRAW MODELS ------- ////
+
 		// Draw our model
 		// view/projection transformations
-
-		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)width / (float)height, nearClip, farClip);
+		defaultShader.use();
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)WIDTH / (float)HEIGHT, nearClip, farClip);
 		glm::mat4 view = camera.GetViewMatrix();
-		shader.setMat4("projection", projection);
-		shader.setMat4("view", view);
+		defaultShader.setMat4("projection", projection);
+		defaultShader.setMat4("view", view);
 
 		// render the loaded model
 		glm::mat4 temp = glm::mat4(1.0f);
-		//temp = glm::rotate(temp, -90.0f, glm::vec3(0.0f, 0.0f, 1.0f));
-		//temp = glm::translate(temp, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
-		//temp = glm::scale(temp, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
-		shader.setMat4("model", temp);
-		
-		model.Draw(shader);
-		player.Draw(shader);
+		defaultShader.setMat4("model", temp);
+		model.Draw(defaultShader);
+		player.Draw(defaultShader);
+
+
+
+		//// ------ DRAW FRAMEBUFFER ------- ////
+
+		// now bind back to default framebuffer and draw a quad plane with the attached framebuffer color texture
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glDisable(GL_DEPTH_TEST); // disable depth test so screen-space quad isn't discarded due to depth test.
+		// clear all relevant buffers
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // set clear color to white (not really necessary actually, since we won't be able to see behind the quad anyways)
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		screenShader.use();
+		glBindVertexArray(quadVAO);
+		glBindTexture(GL_TEXTURE_2D, textureColorbuffer);	// use the color attachment texture as the texture of the quad plane
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+
 
 		// Swap the back buffer with the front buffer
-		glfwSwapBuffers(window);
-
 		// Take care of all GLFW events
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteBuffers(1, &quadVBO);
+	glDeleteFramebuffers(1, &framebuffer);
 
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
